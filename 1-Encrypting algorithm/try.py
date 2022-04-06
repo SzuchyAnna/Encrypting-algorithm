9d2974110856bb0c4701f7ef3df1e91f878e711d
SzuchyAnna <szuchyanna@hotmail.com>
2022-04-06 23:23:07 +0200

    Updated try.py

--- 1-Encrypting algorithm/try.py
+++ 1-Encrypting algorithm/try.py
@@ -2,9 +2,19 @@ from random import choices
 import string
 
 n = 20
-# encrypt_set = string.ascii_lowercase + ' '
+encrypt_set = string.ascii_lowercase + ' '
+# encrypt_set TÍPUSA STRING
+
 # print("'" + encrypt_set + "'")
 # print("'" + ''.join(choices(encrypt_set, k=n)) + "'")
 # print ("this is a string with 'quotes'")
 # print('this is a string with "quotes"')
-print("'" + ''.join(choices(string.ascii_lowercase + ' ', k=n)) + "'")
+
+# IGEN, A SZÓKÖZT IS BELERAKJA
+# print("'" + ''.join(choices(string.ascii_lowercase + ' ', k=n)) + "'")
+
+message = 'hello world'
+# print([i for i in message])
+# print([i in encrypt_set for i in message])
+print([encrypt_set.index(i) for i in message])
+# print(encrypt_set[message[2]])
