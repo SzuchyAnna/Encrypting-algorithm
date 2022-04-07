92c0819d3789f7629cc3cb8e70f955794aa81193
SzuchyAnna <szuchyanna@hotmail.com>
2022-04-07 22:23:31 +0200

    Finishing the core of class Encryper

--- 1-Encrypting algorithm/encrypt.py
+++ 1-Encrypting algorithm/encrypt.py
@@ -5,19 +5,29 @@ import string
 class Encrypter:
     def __init__(self, message):
         self.message = message
+        self.key = self.key_gen()
+        self.encrypted_message = self.encrypter()
 
-    # encrypt_set = string.ascii_lowercase + ' '
+    encrypt_set = string.ascii_lowercase + ' '
 
     def key_gen(self):
         n = len(self.message)
-        return ''.join(choices(string.ascii_lowercase + ' ', k=n))
+        return ''.join(choices(self.encrypt_set, k=n))
 
-    def num_gen(self):
-        encrypt_set = string.ascii_lowercase + ' '
-        return [encrypt_set.index(i) for i in self.message]
+    def num_gen(self, text):
+        # encrypt_set = string.ascii_lowercase + ' '
+        return [self.encrypt_set.index(i) for i in text]
 
     def encrypter(self):
-        pass
+        message_num = self.num_gen(self.message)
+        key_num = self.num_gen(self.key)
+        return [(message_num[i] + key_num[i]) % 26 for i in range(len(message_num))]
+
+    def decrypter(self, encrypted_message, key):
+        encrypted_message_num = self.num_gen(encrypted_message)
+        key_num = self.num_gen(key)
+        message_num = [(encrypted_message_num[i] - key_num[i]) % 26 for i in range(len(encrypted_message_num))]
+        return ''.join([self.encrypt_set[i] for i in message_num])
 
     def input_tester(self):
         # a-z + szóköz
