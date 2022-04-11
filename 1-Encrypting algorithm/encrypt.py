a3f2f7fb42c13ee36e91a9732c953ce58cd27e85
SzuchyAnna <szuchyanna@hotmail.com>
2022-04-11 21:19:47 +0200

    Update encrypt.py

--- 1-Encrypting algorithm/encrypt.py
+++ 1-Encrypting algorithm/encrypt.py
@@ -8,15 +8,15 @@ class Encrypter:
         self.key = self.key_gen()
         self.encrypted_message = self.encrypter()
 
-    encrypt_set = string.ascii_lowercase + ' '
+    char_set = string.ascii_lowercase + ' '
 
     def key_gen(self):
         n = len(self.message)
-        return ''.join(choices(self.encrypt_set, k=n))
+        return ''.join(choices(self.char_set, k=n))
 
     def num_gen(self, text):
         # encrypt_set = string.ascii_lowercase + ' '
-        return [self.encrypt_set.index(i) for i in text]
+        return [self.char_set.index(i) for i in text]
 
     def encrypter(self):
         message_num = self.num_gen(self.message)
@@ -27,7 +27,7 @@ class Encrypter:
         encrypted_message_num = self.num_gen(encrypted_message)
         key_num = self.num_gen(key)
         message_num = [(encrypted_message_num[i] - key_num[i]) % 26 for i in range(len(encrypted_message_num))]
-        return ''.join([self.encrypt_set[i] for i in message_num])
+        return ''.join([self.char_set[i] for i in message_num])
 
     def input_tester(self):
         # a-z + szóköz
