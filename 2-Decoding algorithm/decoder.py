a06cc629e5c8457100ee7d5403cc1fd26a2e0845
SzuchyAnna <szuchyanna@hotmail.com>
2022-05-25 12:40:04 +0200

    Debugging

--- 2-Decoding algorithm/decoder.py
+++ 2-Decoding algorithm/decoder.py
@@ -1,48 +1,49 @@
 import string
 
 
-def decoder(word, encrypted_message_word_s, encrypted_message_other):
+char_set = string.ascii_lowercase + ' '
+
+
+def decoder(word, words_encrypted_message, other_encrypted_message):
     message_1 = word + ' '
-    key = piece_teller(message_1, encrypted_message_word_s)
-    piece = piece_teller(key, encrypted_message_other)
+    key = piece_teller(message_1, words_encrypted_message)
+    piece = piece_teller(key, other_encrypted_message)
     word_list = word_guesser(piece)
     message_2 = ''.join(piece)
-    print(decoding_cycle(piece, word_list, message_2, encrypted_message_word_s, encrypted_message_other))
+    print(decoding_cycle(piece, word_list, message_2, words_encrypted_message, other_encrypted_message))
 
 
-def decoding_cycle(piece, word_list, message_lesser_known, encrypted_message_more, encrypted_message_less):
+def decoding_cycle(piece, word_list, lesser_known_message, encrypted_message_more_known, encrypted_message_lesser_known):
     for x in word_list:
-        inner_message_2 = message_lesser_known + x.replace(''.join(piece), '') + ' '
-        inner_key = piece_teller(inner_message_2, encrypted_message_less)
-        inner_message_1 = ''.join(piece_teller(inner_key, encrypted_message_more))
+        inner_message_2 = lesser_known_message + x.replace(''.join(piece), '') + ' '
+        inner_key = piece_teller(inner_message_2, encrypted_message_lesser_known)
+        inner_message_1 = ''.join(piece_teller(inner_key, encrypted_message_more_known))
         inner_piece = ''.join([i for i in inner_message_1.split(' ')[-1]])
         inner_word_list = word_guesser(inner_piece)
-        if len(inner_key) == len(encrypted_message_less):
+        if len(inner_key) == len(encrypted_message_lesser_known):
             return inner_key, inner_message_1, inner_message_2.strip()
         else:
             if not inner_word_list:
                 pass
             else:
-                decoding_cycle(inner_piece, inner_word_list, inner_message_1, encrypted_message_less, encrypted_message_more)
+                decoding_cycle(inner_piece, inner_word_list, inner_message_1, encrypted_message_lesser_known, encrypted_message_more_known)
 
 
 def word_guesser(word_piece):
-    vocabulary = open('words.txt', 'r')
+    vocabulary = [i.strip() for i in open('words.txt', 'r')]
     wordlist = []
     for i in vocabulary:
-        if i.startswith(word_piece):
-            wordlist.append(i.strip())
+        if i.startswith(''.join(word_piece)):
+            wordlist.append(i)
     return wordlist
 
 
-def piece_teller(m_k, encrypted_message):
-    char_set = string.ascii_lowercase + ' '
+def piece_teller(m_or_k_piece, encrypted_message):
     encrypted_message_num = num_gen(encrypted_message)
-    message_num = num_gen(m_k)
+    message_num = num_gen(m_or_k_piece)
     new_message_num = [(encrypted_message_num[i] - message_num[i]) % 27 for i in range(len(message_num))]
     return [char_set[i] for i in new_message_num]
 
 
 def num_gen(text):
-    char_set = string.ascii_lowercase + ' '
     return [char_set.index(i) for i in text]
